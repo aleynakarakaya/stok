76cacda1a8cd1d1505b69a84659b4bcc9bda592a
aleyna <alynkrky34@gmail.com>
2021-01-10 19:08:54 +0300

    githuba proje yüklüyorum

---
+++ frmSatis.Designer.cs
@@ -0,0 +1,428 @@
+﻿
+namespace Stok_
+{
+    partial class frmSatis
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTc = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtToplamFiyati = new System.Windows.Forms.TextBox();
+            this.txtSatışFiyatı = new System.Windows.Forms.TextBox();
+            this.txtMiktari = new System.Windows.Forms.TextBox();
+            this.txtÜrünAdı = new System.Windows.Forms.TextBox();
+            this.txtBarkodNo = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnSatışİptal = new System.Windows.Forms.Button();
+            this.btnSatışYap = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblGenelToplam = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(262, 144);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(550, 230);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtTc);
+            this.groupBox1.Controls.Add(this.txtTelefon);
+            this.groupBox1.Controls.Add(this.txtAdSoyad);
+            this.groupBox1.Location = new System.Drawing.Point(10, 144);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Müşteri İşlemleri";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Telefon";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ad Soyad";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "TC";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtTc
+            // 
+            this.txtTc.Location = new System.Drawing.Point(83, 19);
+            this.txtTc.Name = "txtTc";
+            this.txtTc.Size = new System.Drawing.Size(100, 20);
+            this.txtTc.TabIndex = 3;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(83, 65);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefon.TabIndex = 3;
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(83, 42);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtToplamFiyati);
+            this.groupBox2.Controls.Add(this.txtSatışFiyatı);
+            this.groupBox2.Controls.Add(this.txtMiktari);
+            this.groupBox2.Controls.Add(this.txtÜrünAdı);
+            this.groupBox2.Controls.Add(this.txtBarkodNo);
+            this.groupBox2.Location = new System.Drawing.Point(10, 267);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 149);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ürün İşlemleri";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(14, 110);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Toplam Fiyat";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Satış Fiyatı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Miktarı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ürün Adı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Barkod No";
+            // 
+            // txtToplamFiyati
+            // 
+            this.txtToplamFiyati.Location = new System.Drawing.Point(83, 107);
+            this.txtToplamFiyati.Name = "txtToplamFiyati";
+            this.txtToplamFiyati.Size = new System.Drawing.Size(100, 20);
+            this.txtToplamFiyati.TabIndex = 9;
+            // 
+            // txtSatışFiyatı
+            // 
+            this.txtSatışFiyatı.Location = new System.Drawing.Point(83, 87);
+            this.txtSatışFiyatı.Name = "txtSatışFiyatı";
+            this.txtSatışFiyatı.Size = new System.Drawing.Size(100, 20);
+            this.txtSatışFiyatı.TabIndex = 7;
+            // 
+            // txtMiktari
+            // 
+            this.txtMiktari.Location = new System.Drawing.Point(83, 67);
+            this.txtMiktari.Name = "txtMiktari";
+            this.txtMiktari.Size = new System.Drawing.Size(100, 20);
+            this.txtMiktari.TabIndex = 5;
+            // 
+            // txtÜrünAdı
+            // 
+            this.txtÜrünAdı.Location = new System.Drawing.Point(83, 47);
+            this.txtÜrünAdı.Name = "txtÜrünAdı";
+            this.txtÜrünAdı.Size = new System.Drawing.Size(100, 20);
+            this.txtÜrünAdı.TabIndex = 3;
+            // 
+            // txtBarkodNo
+            // 
+            this.txtBarkodNo.Location = new System.Drawing.Point(83, 27);
+            this.txtBarkodNo.Name = "txtBarkodNo";
+            this.txtBarkodNo.Size = new System.Drawing.Size(100, 20);
+            this.txtBarkodNo.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(262, 392);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(818, 144);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnSatışİptal
+            // 
+            this.btnSatışİptal.Location = new System.Drawing.Point(818, 173);
+            this.btnSatışİptal.Name = "btnSatışİptal";
+            this.btnSatışİptal.Size = new System.Drawing.Size(75, 23);
+            this.btnSatışİptal.TabIndex = 5;
+            this.btnSatışİptal.Text = "Satış İptal";
+            this.btnSatışİptal.UseVisualStyleBackColor = true;
+            // 
+            // btnSatışYap
+            // 
+            this.btnSatışYap.Location = new System.Drawing.Point(737, 392);
+            this.btnSatışYap.Name = "btnSatışYap";
+            this.btnSatışYap.Size = new System.Drawing.Size(75, 23);
+            this.btnSatışYap.TabIndex = 6;
+            this.btnSatışYap.Text = "Satış Yap";
+            this.btnSatışYap.UseVisualStyleBackColor = true;
+            this.btnSatışYap.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(507, 403);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Genel Toplam";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // lblGenelToplam
+            // 
+            this.lblGenelToplam.AutoSize = true;
+            this.lblGenelToplam.Location = new System.Drawing.Point(547, 423);
+            this.lblGenelToplam.Name = "lblGenelToplam";
+            this.lblGenelToplam.Size = new System.Drawing.Size(0, 13);
+            this.lblGenelToplam.TabIndex = 8;
+            this.lblGenelToplam.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(903, 100);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(689, 28);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(123, 51);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "Satışları Listeleme";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(485, 28);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(123, 51);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Ürün Listeleme";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(356, 28);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(123, 51);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Üürün Ekleme";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(176, 28);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 51);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Müşteri Listeleme";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(47, 28);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 51);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Müşteri Ekleme";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // frmSatis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(903, 515);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblGenelToplam);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnSatışYap);
+            this.Controls.Add(this.btnSatışİptal);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmSatis";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Satış Sayfası";
+            this.Load += new System.EventHandler(this.frmSatis_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTc;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtToplamFiyati;
+        private System.Windows.Forms.TextBox txtSatışFiyatı;
+        private System.Windows.Forms.TextBox txtMiktari;
+        private System.Windows.Forms.TextBox txtÜrünAdı;
+        private System.Windows.Forms.TextBox txtBarkodNo;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnSatışİptal;
+        private System.Windows.Forms.Button btnSatışYap;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblGenelToplam;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ frmSatis.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stok_
+{
+    public partial class frmSatis : Form
+    {
+        public frmSatis()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmSatis_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            frmMüşteriEkle ekle = new frmMüşteriEkle();
+            ekle.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            frmMüşteriListele listele = new frmMüşteriListele();
+            listele.ShowDialog();
+        }
+    }
+}
